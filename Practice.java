f1b3c23fa73ba5dcd6f81de20d4b6c9cdfac42ca
Govind N Acharya <govindnacharya@gmail.com>
2026-01-13 15:19:35 +0530

    [Refactor]Add . Created Calculate Function

--- Practice.java
+++ Practice.java
@@ -2,4 +2,8 @@ public class Practice{
     public static void main(String args[]){
         System.out.println("Hello world");
     }
+
+    public static void calculate(int a, int b){
+        System.out.println(a * b);
+    }
 }
