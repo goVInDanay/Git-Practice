58c83e4ad9f370784a3fc9a96b3207f594e3c947
Govind N Acharya <govindnacharya@gmail.com>
2026-01-13 15:59:54 +0530

    [Govind N Acharya] Add. Added addition functionality

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public static void main(String args[]) {
         System.out.println("Hello world");
     }
+
+    public static int addition(int a, int b){
+        return a + b;
+    }
 }
