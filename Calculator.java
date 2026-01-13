63df847257ca7cb6b61c16ec406d4b505a597c7a
Govind N Acharya <govindnacharya@gmail.com>
2026-01-13 16:19:03 +0530

    [Govind N Acharya] Fixed . Fixed merge conflicts

--- Calculator.java
+++ Calculator.java
@@ -2,14 +2,4 @@ public class Calculator {
     public static void main(String args[]) {
         System.out.println("Hello world");
     }
-
-    public static int addition(int a, int b){
-        System.out.println("Addition");
-        return a + b;
-    }
-    
-    public static int subtraction(int a, int b){
-        System.out.println("Subtraction");
-        return a - b;
-    }
 }
