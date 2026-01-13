8a7bfd2322d62cecfe21f462f18e5d4c32606b7f
Govind N Acharya <govindnacharya@gmail.com>
2026-01-13 16:24:21 +0530

    [Govind N Acharya] Add. Added multiplication functionality

--- Calculator.java
+++ Calculator.java
@@ -2,16 +2,8 @@ public class Calculator {
     public static void main(String args[]) {
         System.out.println("Hello world");
     }   
-    public static int addition(int a, int b){
-        System.out.println("Addition");
-        return a + b;
-    }
     public static int multiplication(int a, int b){
         System.out.println("Multiplication");
         return a * b;
-    }
-    public static int subtraction(int a, int b){
-        System.out.println("Subtraction");
-        return a - b;
     }
 }
