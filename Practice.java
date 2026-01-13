f6dc370f08d4e90add88703d0572a628d3fc3d5f
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-01-13 15:27:16 +0530

    [Arpit Gupta] Refactor . Provided calculate method

--- Practice.java
+++ Practice.java
@@ -1,5 +1,10 @@
 public class Practice{
     public static void main(String args[]){
         System.out.println("Hello world");
+        calculate();
+    }
+    
+    static void calculate() {
+    	System.out.println("Calculation done");
     }
 }
